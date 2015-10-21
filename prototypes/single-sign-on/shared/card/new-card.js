6624c439fd3e456a22187fbbcf61008b0f5894bc
chris.haas <chris.haas@servicenow.com>
2015-10-21 09:58:44 -0700

    Card polish

--- prototypes/single-sign-on/shared/card/new-card.js
+++ prototypes/single-sign-on/shared/card/new-card.js
@@ -4,12 +4,13 @@
         return {
             restrict: "AE",
             scope: {
-                url: "@"
+                url: "@",
+                text: "@"
             },
             template: 
                 "<a href='{{ url }}' class='card card-add'>" +
                     "<div class='card-content'>" + 
-                        "<span class='card-title'><span class='icon-add-circle-empty' /> Add New</span>" +
+                        "<span class='card-title'><span class='icon-add-circle-empty add-new-icon' /> {{ text }}</span>" +
                     "</div>" +
                 "</a>"
         };
