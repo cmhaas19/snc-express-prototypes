1c4712616cc0e1b017722426ab769c59081daac1
chris.haas <chris.haas@servicenow.com>
2016-11-16 17:05:26 -0800

    adding grid and counts

--- prototypes/plugin-dependency/controller.js
+++ prototypes/plugin-dependency/controller.js
@@ -3,16 +3,68 @@
     var controller = function (pluginService, $timeout) {
         var ctrl = this;
 
+        ctrl.plugins = [];
+        ctrl.selectedCount = 0;
+        ctrl.selectedDependencyCount = 0;      
+        ctrl.selectedPlugins = [];  
+
         var dataSource = new kendo.data.DataSource({
             transport: {
                 read: function (e) {
                 	pluginService.getPlugins().then(function(plugins) {
                 		ctrl.plugins = plugins;
-                		$timeout(function () { e.success(plugins); }, 200);
+
+                        var data = _.map(plugins, function(p){
+                            return {
+                                name: p.name,
+                                id: p.id,
+                                description: p.description,
+                                sms: p.sms,
+                                express: p.express,
+                                depcount: angular.isArray(p.dependencies) ? p.dependencies.length : 0
+                            };
+                        });
+                		$timeout(function () { e.success(data); }, 200);
                 	});
                 }
             }
         });
+
+        var findPlugin = function(id) {
+            return _.find(ctrl.plugins, function(p) { return p.id == id; });
+        };
+
+        var updateCounts = function() {
+            var count = 0;
+
+            ctrl.selectedPlugins.forEach(function(p){
+                count += angular.isArray(p.dependencies) ? p.dependencies.length : 0;
+            });
+
+            ctrl.selectedCount = ctrl.selectedPlugins.length;
+            ctrl.selectedDependencyCount = count;  
+        };
+
+        var addSelectedPlugin = function(plugin) {
+            var existingPlugin = _.find(ctrl.selectedPlugins, function(p){ return p.id == plugin.id; });
+            if(existingPlugin == undefined)
+                ctrl.selectedPlugins.push(plugin);
+        };
+
+        var removeSelectedPlugin = function(plugin) {
+            _.remove(ctrl.selectedPlugins, function(p){ return p.id == plugin.id; });
+        };
+
+        ctrl.selectPlugin = function(plugin) {                   
+            var p = findPlugin(plugin.id);
+
+            if(plugin.selected)
+                addSelectedPlugin(p);
+            else
+                removeSelectedPlugin(p);
+
+            updateCounts();
+        };
 
         ctrl.gridOptions = {
         	autoBind: true,
@@ -23,8 +75,18 @@
                 mode: "single",
                 allowUnsort: false
             },
-            pageable: false,
+            pageable: {
+                numeric: false,
+                previousNext: false,
+                messages: {
+                    display: "Total: {2}"
+                }
+            },
             columns: [{
+                    title: "",
+                    template: '<input ng-model="dataItem.selected" type="checkbox" ng-change="ctrl.selectPlugin(dataItem)"></input>',
+                    width: 30
+                },{
                     field: "name",
                     title: "Name"
                 }, {
@@ -34,11 +96,17 @@
                     field: "description",
                     title: "Description"
                 },{
+                    field: "depcount",
+                    title: "# Deps",
+                    width: 65
+                },{
                     field: "sms",
-                    title: "SMS?"
+                    title: "SMS?",
+                    width: 65
                 },{
                     field: "express",
-                    title: "Express?"
+                    title: "Express?",
+                    width: 80
                 }
             ],
             dataSource: dataSource
--- prototypes/plugin-dependency/service.js
+++ prototypes/plugin-dependency/service.js
@@ -6,14 +6,62 @@
 
         var baseUrl = "https://empchaas1.service-now.com/";
 
-        var getPlugins = function() {
-            return $http.get(baseUrl + "api/now/table/dpp_plugin").then(function(response){
+        var getPluginDependencies = function() {
+            var params = {
+                method: "get",                
+                url: baseUrl + "api/now/table/dpp_plugin_dependency",
+                params: {
+                    sysparm_query: "ORDERBYplugin_id",
+                }
+            };
+
+            return $http(params).then(function(response){
                 return response.data.result;
             });
         };
 
+        var getPlugins = function() {
+            var params = {
+                method: "get",                
+                url: baseUrl + "api/now/table/dpp_plugin",
+                params: {
+                    sysparm_query: "ORDERBYname",
+                }
+            };
+
+            return $http(params).then(function(response){
+                return response.data.result;
+            });
+        };
+
+        var getPluginsWithDependencies = function() {
+            return $q.all([getPlugins(), getPluginDependencies()]).then(function(results) {
+                var plugins = results[0],
+                    dependencies = results[1];
+                
+                plugins.forEach(function(plugin) {
+
+                    var dependentPlugins = _.filter(dependencies, function(d){ return d.plugin_id == plugin.id; });
+
+                    if(dependentPlugins && dependentPlugins.length) {
+                        dependentPlugins.forEach(function(dependency) {
+                            var p = _.find(plugins, function(t) { return t.id == dependency.required_plugin_id; });
+
+                            if(p) {
+                                if(angular.isUndefined(plugin.dependencies))
+                                    plugin.dependencies = [];
+                                plugin.dependencies.push(p);
+                            }
+                        });
+                    }
+                });
+
+                return plugins;
+            });
+        };
+
         return {
-            getPlugins: getPlugins
+            getPlugins: getPluginsWithDependencies
         };
 
     };
